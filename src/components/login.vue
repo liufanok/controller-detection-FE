6f1ccfb3efa7d43b0a716bbc241f1e5718c8975e
qinsong <qinsong007@outlook.com>
2018-04-04 17:00:53 +0800

    登陆

---
+++ src/components/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+      <el-input v-model="name" placeholder="请输入用户名"></el-input>
+       <el-input v-model="pwd" placeholder="请输入密码"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      name:'',
+      pwd:''
+    }
+  },
+  mounted () {
+      this.axios.post(url,{name:this.name,pwd:this.pwd}).then(res=>{
+
+      }).catch(error=>{
+        
+      })
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(VueAxios, axios)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
