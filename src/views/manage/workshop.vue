11dfa720bc167b1a4d57509282aca1cf8acc50d3
liufan <540939842@qq.com>
2018-05-16 23:33:58 +0800

    中英文翻译

--- src/views/manage/workshop.vue
+++ src/views/manage/workshop.vue
@@ -238,7 +238,7 @@ export default {
       })
     },
     plantDelete(val) {
-      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
